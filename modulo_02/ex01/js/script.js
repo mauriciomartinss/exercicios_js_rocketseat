815a25b9989202016046103900d76d8a16f682ce
mauriciomartinss <mauricio.mmd.damasceno@gmail.com>
2020-09-22 21:12:38 -0300

    primeiro exercicio do modulo 2 finalizado

---
+++ modulo_02/ex01/js/script.js
@@ -0,0 +1,9 @@
+// criar um botão que ao ser clicado, executará uma função que gera um quadrado novo
+
+const btn = document.querySelector('#generate') 
+let container = document.querySelector('#container')
+
+btn.addEventListener('click', function() {
+    container.insertAdjacentHTML('afterend', '<div id="square"></div>')
+    
+})
